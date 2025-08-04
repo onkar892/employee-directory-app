5377049944898664274463d58679ed427cefa8b1
Onkar Salunke <onkarsalunke9@gmail.com>
2025-08-04 10:17:03 +0530

    modified code

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,22 +1,26 @@
-# deploy.ps1
-
 Write-Output "Authenticating with Azure..."
 
-# Login using service principal credentials passed as environment variables
 az login --service-principal `
     --username $env:AZURE_CLIENT_ID `
     --password $env:AZURE_CLIENT_SECRET `
-    --tenant $env:AZURE_TENANT_ID
+    --tenant $env:AZURE_TENANT_ID | Out-Null
+
+Write-Output "Checking if resource group exists..."
+$exists = az group exists --name $env:AZURE_RESOURCE_GROUP | ConvertFrom-Json
+if (-not $exists) {
+    Write-Error "ERROR: Resource group '$env:AZURE_RESOURCE_GROUP' not found."
+    exit 3
+}
 
 Write-Output "Deploying to Azure App Service..."
 
 az webapp config container set `
     --name $env:AZURE_APP_SERVICE `
     --resource-group $env:AZURE_RESOURCE_GROUP `
-    --docker-custom-image-name $env:DOCKER_IMAGE `
-    --docker-registry-server-url https://index.docker.io `
-    --docker-registry-server-user $env:DOCKER_USER `
-    --docker-registry-server-password $env:DOCKER_PASS
+    --container-image-name $env:DOCKER_IMAGE `
+    --container-registry-url https://index.docker.io `
+    --container-registry-user $env:DOCKER_USER `
+    --container-registry-password $env:DOCKER_PASS
 
 az webapp restart --name $env:AZURE_APP_SERVICE --resource-group $env:AZURE_RESOURCE_GROUP
 
